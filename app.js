454670fd7590940e4c2a6fb42f80a41a1477f4a6
Matthew Brunkhorst <mbrunkhorst@matthews-mbp-7.lan>
2023-08-17 10:52:31 -0700

    a6 rest su23

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
-import TuitsController from "./controllers/tuits/tuits-controller.js";
+//import TuitsController from "./controllers/tuits/tuits-controller.js";
+import TuitsController from "./tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
 import cors from 'cors'
 import session from "express-session";
+import mongoose from "mongoose";
+const CONNECTION_STRING = 'mongodb+srv://mbrunkhorst219:mGCsboguLFqP4dQZ@cluster0.xwuxckj.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/tuiter'
+
+//mongoose.connect(CONNECTION_STRING);
+ mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 const app = express()
 app.use(cors({
     credentials: true,
